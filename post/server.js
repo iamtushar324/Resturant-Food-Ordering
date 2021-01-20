9a420589c1fdc1be99d3dfdc093df8810003caf5
Tushar <iamtushar324@gmail.com>
2021-01-20 18:36:15 +0530

    Added skaffold support and ingress-srv with kubernetes
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- post/server.js
+++ post/server.js
@@ -1,9 +1,9 @@
 const app = require("express")();
 
-app.get("/", (req, res) => {
+app.get("/api/post", (req, res) => {
   res.send("post");
 });
 
-app.listen(6666, () => {
-  console.log("Post Service is running on 6666");
+app.listen(3535, () => {
+  console.log("Post Service is running on 3535");
 });
--- query/server.js
+++ query/server.js
@@ -1,9 +1,10 @@
 const app = require("express")();
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
+app.get("/api/query", (req, res) => {
   res.send("hii");
 });
 
-app.listen(4444, () => {
-  console.log("Query Service is running on 4444");
+app.listen(4545, () => {
+  console.log("Query Service is running on 4545");
 });
